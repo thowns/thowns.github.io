e63e98452f6ed4db77db55ec30dc35d0f1e4c180
Thomas <thomas>
2023-08-21 23:14:14 +1000

    initial commit

---
+++ src/PhotoPanels.jsx
@@ -0,0 +1,78 @@
+import * as imgs from "./imgs";
+
+const panels = [
+  {
+    image: imgs.newborn2,
+    date: new Date("2022-10-15"),
+    children: null,
+  },
+  {
+    image: imgs.santa,
+    date: new Date("2022-11-29"),
+    children: null,
+  },
+  {
+    image: imgs.rabbit,
+    date: new Date("2022-11-29"),
+    children: null,
+  },
+  {
+    image: imgs.sheep,
+    date: new Date("2023-01-14"),
+    children: null,
+  },
+  {
+    image: imgs.rock_hand,
+    date: new Date("2023-02-13"),
+    children: null,
+  },
+  {
+    image: imgs.saucer,
+    date: new Date("2023-04-06"),
+    horizontalAlign: "end",
+    children: null,
+  },
+  {
+    image: imgs.hundred_day,
+    date: new Date("2023-01-22"),
+    children: null,
+  },
+  {
+    image: imgs.tiger,
+    date: new Date("2023-01-22"),
+    children: null,
+  },
+  {
+    image: imgs.jordan,
+    date: new Date("2023-02-19"),
+    horizontalAlign: "end",
+    children: null,
+  },
+  {
+    image: imgs.balloon,
+    date: new Date("2023-02-14"),
+    horizontalAlign: "end",
+    children: null,
+  },
+  {
+    image: imgs.cap,
+    date: new Date("2023-02-04"),
+    children: null,
+  },
+  {
+    image: imgs.country_road,
+    date: new Date("2023-07-30"),
+    children: null,
+  },
+  {
+    image: imgs.beauty,
+    date: new Date("2023-07-28"),
+    children: null,
+  },
+  {
+    image: imgs.smile,
+    date: new Date("2022-12-08"),
+  }
+];
+
+export default panels;
---
+++ src/imgs/index.js
@@ -0,0 +1,36 @@
+import hat from "./hat.jpeg";
+import hundred_day from "./100.jpeg";
+import cap from "./cap.jpeg";
+import jordan from "./jordan.jpeg";
+import newborn2 from "./newborn2.jpeg";
+import santa from "./santa.jpeg";
+import tiger from "./tiger.jpeg";
+import balloon from "./balloon.jpeg";
+import beauty from "./beauty.jpeg";
+import country_road from "./country_road.jpeg";
+import rabbit from "./rabbit.jpeg";
+import rock_hand from "./rock_hand.jpeg";
+import saucer from "./saucer.jpeg";
+import sheep from "./sheep.jpeg";
+import smile from "./smile.jpeg";
+
+import gif_main from "./main.gif";
+
+export {
+  hat,
+  hundred_day,
+  cap,
+  jordan,
+  newborn2,
+  santa,
+  tiger,
+  balloon,
+  beauty,
+  country_road,
+  rabbit,
+  rock_hand,
+  saucer,
+  sheep,
+  smile,
+  gif_main,
+};
